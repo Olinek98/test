74ba11a409f778dfe426dc6c9a82f2958f1e8056
Olinek98 <o.zochowska@gmail.com>
2022-03-14 18:28:05 +0100

    piaskownica git start

---
+++ powtorka.py
@@ -0,0 +1,29 @@
+import requests
+orangutan = 'https://zajecia-programowania-xd.pl/flagi'
+surowe_info = requests.get( orangutan)
+text = surowe_info.text
+
+# Cel:
+# Lista linków (wszytkich).
+
+# przykładowe rozwiązanie:
+lista = [
+    "link1.pl",
+    "link2.pl",
+    "link3.pl"
+]
+
+# Przygotowanie:
+lista_linii = text.split('</p>')
+
+# Realizacja :)
+linki = []
+for linia in lista_linii:
+    link = linia.replace('<p>', '')
+    link = link.replace('- ', '')
+    link = link.strip()
+    
+    if ' ' in link or '<' in link:
+        continue
+linki.append(link)
+   
